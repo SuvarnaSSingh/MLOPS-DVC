d78a0e01600201343065f6c741df1550a58088ab
SuvarnaSSingh <Suvarna.ozarkar@gmail.com>
2024-03-20 16:29:09 -0400

    loaded data in raw folders

--- src/get_data.py
+++ src/get_data.py
@@ -17,7 +17,7 @@ def get_data(config_path):
     config=read_params(config_path)
     data_path=config["data_source"]["s3_source"]
     df=pd.read_csv(data_path,sep=",",encoding='utf-8')
-    print(df.head())
+    return df 
 
 
 
@@ -25,4 +25,4 @@ if __name__=="__main__":
     args=argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parsed_arg=args.parse_args()
-    get_data(config_path=parsed_arg.config)
+    data=get_data(config_path=parsed_arg.config)
